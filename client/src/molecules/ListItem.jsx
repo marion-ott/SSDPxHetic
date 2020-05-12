a635860069b09c65c362eaaf536d5558e916d35d
Simon <soleausimon@hotmail.fr>
2020-05-12 15:03:45 +0200

    [FRONT] vertical align on css

--- client/src/molecules/ListItem.jsx
+++ client/src/molecules/ListItem.jsx
@@ -4,15 +4,15 @@ import Icon from '../atoms/Icon'
 
 const ListItem = ({ index, data, keys }) => (
   <tr>
-    <th style={{ verticalAlign: "middle" }}>{index}</th>
+    <th>{index}</th>
     {keys.map((el, i) => {
       if (typeof data[el.name] == 'object') {
-        return <td style={{ verticalAlign: "middle" }} key={i}>{data[el.name] ? data[el.name].zone : 'N/A'}</td>
+        return <td key={i}>{data[el.name] ? data[el.name].zone : 'N/A'}</td>
       }
       if (el.name === 'lastVisit') {
-        return <td style={{ verticalAlign: "middle" }} key={i}>{new Date(data[el.name]).toLocaleDateString()}</td>
+        return <td key={i}>{new Date(data[el.name]).toLocaleDateString()}</td>
       }
-      return <td style={{ verticalAlign: "middle" }} key={i}>{data[el.name]}</td>
+      return <td key={i}>{data[el.name]}</td>
     })}
     <td>
       <Dropdown title={<Icon classname='fa-ellipsis-h' />} data={["Détails", "Supprimer"]} />
