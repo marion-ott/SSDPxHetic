1838a9863dd81eeba04a0dff5b76d6e5e307178a
ynohtn <anthonyrnd69@gmail.com>
2020-05-15 14:21:03 +0200

    [FEATURE] LOGIN + userContext added

--- client/src/views/Login.jsx
+++ client/src/views/Login.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import * as yup from 'yup'
 import { useMutation } from '@apollo/react-hooks'
 import { LOGIN } from './../graphql/mutations/auth'
 import { getFormProps } from './../global/data'
 import { Form } from './../organisms'
 import { Logo } from './../atoms'
+import UserContext from '../context/userContext'
 
 const schema = yup.object({
   email: yup.string().email('Email invalide').required('Email requis'),
   password: yup.string().required('Mot de passe requis')
 })
 
-const Login = () => {
+const Login = ({ handleLogin }) => {
   const [login, { client, loading, error }] = useMutation(LOGIN, {
     onCompleted({ login }) {
       //TODO: update context with logged login.user
+      handleLogin(login.user)
+      console.log(login)
       localStorage.setItem('token', login.token)
       client.resetStore()
     },
@@ -25,6 +28,10 @@ const Login = () => {
     email: '',
     password: ''
   })
+
+  const user = useContext(UserContext)
+
+  // console.log()
 
   if (error) {
     return <p>there was an error</p>
