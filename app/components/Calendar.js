4f3ca1d86795961403a66d04f7601d8aeca3413f
Marion <marion_ott@me.com>
2020-06-05 10:37:54 +0200

    feat(calendar): update calendar locale

--- app/components/Calendar.js
+++ app/components/Calendar.js
@@ -1,6 +1,56 @@
 import React, { Fragment, useState } from 'react'
 import { View } from 'react-native'
-import { Calendar } from '@ui-kitten/components'
+import { Calendar, NativeDateService } from '@ui-kitten/components'
+
+const i18n = {
+  dayNames: {
+    short: ['Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam', 'Dim'],
+    long: [
+      'Lundi',
+      'Mardi',
+      'Mercredi',
+      'Jeudi',
+      'Vendredi',
+      'Samedi',
+      'Dimanche'
+    ]
+  },
+  monthNames: {
+    short: [
+      'Jan',
+      'Fév',
+      'Mar',
+      'Avr',
+      'Mai',
+      'Juin',
+      'Juil',
+      'Août',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Déc'
+    ],
+    long: [
+      'Janvier',
+      'Février',
+      'Mars',
+      'Avril',
+      'Mai',
+      'Juin',
+      'Juillet',
+      'Août',
+      'Septembre',
+      'Octobre',
+      'Novembre',
+      'Décembre'
+    ]
+  }
+}
+
+const localeDateService = new NativeDateService('fr', {
+  i18n,
+  startDayOfWeek: 0
+})
 
 export default function CalendarElement() {
   const [date, setDate] = useState(new Date())
@@ -8,7 +58,11 @@ export default function CalendarElement() {
   return (
     <View>
       {/* <Text category='h6'>Selected date: {date.toLocaleDateString()}</Text> */}
-      <Calendar date={date} onSelect={(nextDate) => setDate(nextDate)} />
+      <Calendar
+        dateService={localeDateService}
+        date={date}
+        onSelect={(nextDate) => setDate(nextDate)}
+      />
     </View>
   )
 }
