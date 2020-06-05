a28c8f555c750db0e5f72c8eba5d40b4aa8d2376
Marion <marion_ott@me.com>
2020-06-05 10:28:39 +0200

    org(suivi): push current updates

---
+++ app/components/Card.js
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import { Text } from 'react-native'
+
+export function Card(props) {
+  return <Text {...props} />
+}
--- app/components/StyledText.js
+++ app/components/StyledText.js
@@ -1,6 +1,11 @@
-import * as React from 'react';
-import { Text } from 'react-native';
+import * as React from 'react'
+import { Text } from 'react-native'
 
 export function MonoText(props) {
-  return <Text {...props} style={[props.style, { fontFamily: 'space-mono' }]} />;
+  return (
+    <Text
+      {...props}
+      style={[props.style, { fontFamily: `roboto-${props.weight}` }]}
+    />
+  )
 }
