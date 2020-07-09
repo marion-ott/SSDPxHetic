2e51a9de272a75774d277eb9c3aa08fe9b431022
Marion <marion_ott@me.com>
2020-07-10 01:08:15 +0200

    fix(error): replace api url & fix render issue

--- app/screens/NotificationScreen.js
+++ app/screens/NotificationScreen.js
@@ -22,7 +22,9 @@ export default function NotificationScreen() {
             })}
           </ScrollView>
         </>
-      ) : <IsEmpty />)}
+      ) : (
+        <IsEmpty />
+      )}
     </CustomScrollView>
   )
 }
